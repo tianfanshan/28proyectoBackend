6e49d5cb1b7b349f3d448f309a3975b11b00db8e
alextebbitt <alex.tebbitt87@gmail.com>
2022-05-20 22:17:24 +0200

    orders

---
+++ controllers/OrderConroller.js
@@ -0,0 +1,13 @@
+const { Order } = require("../models/index");
+
+const OrderController = {
+  create(req, res) {
+    Order.create({ ...req.body })
+      .then(order =>
+        res.status(201).send({ message: "categoria creado", order })
+      )
+      .catch(console.error);
+  },
+};
+
+module.exports = OrderController;
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8000;
 
 app.use(express.json());
-
+app.use("/order", require("./routes/orders"));
 app.use("/category", require("./routes/categories"));
 
 //prueba para branch
