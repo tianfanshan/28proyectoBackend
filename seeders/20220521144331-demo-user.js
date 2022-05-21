55df180086f760ad5208f5b29dfd849cf15f0633
alextebbitt <alex.tebbitt87@gmail.com>
2022-05-21 17:17:54 +0200

    5 seeders created

--- seeders/20220521144331-demo-user.js
+++ seeders/20220521144331-demo-user.js
@@ -2,21 +2,55 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-   return queryInterface.bulkInsert ( 'Users', [
-
-{
-
-first_name: 'John',
-last_name: 'Smith',
-email: 'example@example.com',
-password:'123456',
-role:'user',
-createdAt: new Date(),
-updatedAt: new Date()
-}
-])
+   return queryInterface.bulkInsert("Users", [
+     {
+       first_name: "John",
+       last_name: "Smith",
+       email: "john@example.com",
+       password: "123456",
+       role: "user",
+       createdAt: new Date(),
+       updatedAt: new Date(),
+     },
+     {
+       first_name: "Kevin",
+       last_name: "Johnstone",
+       email: "kevin@example.com",
+       password: "1234567",
+       role: "user",
+       createdAt: new Date(),
+       updatedAt: new Date(),
+     },
+     {
+       first_name: "Joanna",
+       last_name: "Smitherson",
+       email: "Joanna@example.com",
+       password: "1234568",
+       role: "user",
+       createdAt: new Date(),
+       updatedAt: new Date(),
+     },
+     {
+       first_name: "Bob",
+       last_name: "Boots",
+       email: "example@example.com",
+       password: "1234569",
+       role: "Admin",
+       createdAt: new Date(),
+       updatedAt: new Date(),
+     },
+     {
+       first_name: "Ben",
+       last_name: "Dover",
+       email: "ben@example.com",
+       password: "12345611",
+       role: "user",
+       createdAt: new Date(),
+       updatedAt: new Date(),
+     },
+   ]);
 },
-  },
+  
 
   async down (queryInterface, Sequelize) {
     /**
