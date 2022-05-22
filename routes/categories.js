2b24b751ccfc2c92129f1fd7bc29aeeb1b358408
alextebbitt <alex.tebbitt87@gmail.com>
2022-05-22 18:01:37 +0200

    CRUD end point en categories

--- routes/categories.js
+++ routes/categories.js
@@ -5,4 +5,7 @@ const CategoryController = require("../controllers/CategoryController");
 router.post("/", CategoryController.create);
 router.get("/id/:id", CategoryController.getById);
 router.get("/getbyname/:name", CategoryController.getByName);
+router.delete("/delete/:id", CategoryController.deleteCategory);
+router.put("/update/:id", CategoryController.updateCategory);
+router.get("/getall/", CategoryController.getAll);
 module.exports = router;
