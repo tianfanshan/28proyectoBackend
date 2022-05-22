d6b4d297b1a39f7cce2e0e2dd07b52e929011f85
alextebbitt <alex.tebbitt87@gmail.com>
2022-05-22 14:50:33 +0200

    update getbyname end point

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,4 +1,5 @@
-const { Category, Product } = require("../models/index");
+const { Category, Product, Sequelize } = require("../models/index");
+const { Op } = Sequelize;
 
 const CategoryController = {
   create(req, res) {
@@ -11,9 +12,7 @@ const CategoryController = {
   //Crea un endpoint que devuelva una categoría por id
 
   getById(req, res) {
-    Category.findByPk(req.params.id, {
-        
-    })
+    Category.findByPk(req.params.id, {})
       .then((categories) => res.send(categories))
       .catch((err) => {
         console.log(err);
@@ -23,20 +22,29 @@ const CategoryController = {
       });
   },
 
-
-  // Filtro para buscar categoría por nombre
   getByName(req, res) {
     Category.findOne({
-      where: { name: req.params.name },
-    })
-      .then((categories) => res.send(categories))
-      .catch((err) => {
-        console.log(err);
-        res.status(500).send({
-          message: "There was a problem. :(",
-        });
-      });
+      where: {
+        name: {
+          [Op.like]: `%${req.params.name}%`,
+        },
+      },
+    }).then((post) => res.send(post));
   },
 };
+  // Filtro para buscar categoría por nombre second try 
+//   getByName(req, res) {
+//     Category.findOne({
+//       where: { name: req.params.name },
+//     })
+//       .then((categories) => res.send(categories))
+//       .catch((err) => {
+//         console.log(err);
+//         res.status(500).send({
+//           message: "There was a problem. :(",
+//         });
+//       });
+//   },
+
 
 module.exports = CategoryController;
