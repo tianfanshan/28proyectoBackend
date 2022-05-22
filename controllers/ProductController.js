a17883493022cfa29af7fd6fa777c0cf8b9fb297
alextebbitt <alex.tebbitt87@gmail.com>
2022-05-22 12:30:24 +0200

    Endpoint que traiga un producto por su id HECHO

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,11 +1,26 @@
 const { Product } = require('../models/index')
 
-const ProductController={
-    create(req,res){
-        Product.create({...req.body})
-        .then(product=>res.status(201).send({message:"Product created!",product}))
-        .catch(console.error)
-    }
-}
+const ProductController = {
+  create(req, res) {
+    Product.create({ ...req.body })
+      .then((product) =>
+        res.status(201).send({ message: "Product created!", product })
+      )
+      .catch(console.error);
+  },
+  // Endpoint que traiga un producto por su id
+  getProductById(req, res) {
+    Product.findOne({
+      where: { id: req.params.id },
+    })
+      .then((products) => res.send(products))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send({
+          message: "There was a problem. :(",
+        });
+      });
+  },
+};
 
 module.exports = ProductController;
--- routes/products.js
+++ routes/products.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const ProductController = require('../controllers/ProductController')
 
 router.post('/',ProductController.create);
+router.get("/:id", ProductController.getProductById);
 
 module.exports = router;
