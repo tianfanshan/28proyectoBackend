554c41a5241d940ab37390bb27df8ca758185b9a
alextebbitt <alex.tebbitt87@gmail.com>
2022-05-23 17:20:42 +0200

    authentications

--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const CategoryController = require("../controllers/CategoryController");
+const { authentication } = require("../middleware/authentication");
 
-router.post("/", CategoryController.create);
-router.get("/id/:id", CategoryController.getById);
-router.get("/getbyname/:name", CategoryController.getByName);
-router.delete("/delete/:id", CategoryController.deleteCategory);
-router.put("/update/:id", CategoryController.updateCategory);
-router.get("/getall/", CategoryController.getAll);
+router.post("/", authentication, CategoryController.create);
+router.get("/id/:id", authentication, CategoryController.getById);
+router.get("/getbyname/:name", authentication, CategoryController.getByName);
+router.delete("/delete/:id", authentication, CategoryController.deleteCategory);
+router.put("/update/:id", authentication, CategoryController.updateCategory);
+router.get("/getall/",  authentication, CategoryController.getAll);
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -4,8 +4,8 @@ const ProductController = require('../controllers/ProductController');
 const { authentication, isAdmin } = require('../middleware/authentication');
 
 router.post('/', authentication, ProductController.create);
-router.get("/id/:id", ProductController.getProductById)
-router.get('/name/:name',ProductController.getOneByName)
+router.get("/id/:id", authentication, ProductController.getProductById)
+router.get('/name/:name', authentication, ProductController.getOneByName)
 router.delete('/id/:id', authentication, isAdmin, ProductController.delete);
 router.get('/string/:name',ProductController.getOneByString);
 router.put('/update/:id',authentication, ProductController.update);
